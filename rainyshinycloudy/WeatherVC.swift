4eb368646e61f9e801338e6d1e70274c1b8a85d6
fiona <fi.carty@gmail.com>
2016-12-29 14:14:48 -0800

    doodling around with views in storyboard..

---
+++ rainyshinycloudy/WeatherVC.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherVC.swift
+//  rainyshinycloudy
+//
+//  Created by Fiona Carty on 12/22/16.
+//  Copyright © 2016 Fiona Carty. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+}
+
