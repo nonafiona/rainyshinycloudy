bcc517fc9e054a69494db2ca81989a9b9fcc5c64
fiona <fi.carty@gmail.com>
2016-12-31 01:54:07 -0800

    got dummy data to show in the table view cell :-)

--- rainyshinycloudy/WeatherVC.swift
+++ rainyshinycloudy/WeatherVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController , UITableViewDelegate, UITableViewDataSource{
 
     
     @IBOutlet weak var currentDateLabel: UILabel!
@@ -16,14 +16,27 @@ class WeatherVC: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var currentWeatherImage: UIImageView!
     @IBOutlet weak var currentWeatherTypeLabel: UILabel!
-    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
         
-        
-        
+        return cell
     }
 }
 
