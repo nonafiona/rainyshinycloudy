d2895a08ee1668c611a9ae41591c4d75db784d19
fiona <fi.carty@gmail.com>
2017-01-03 13:29:32 -0800

    data incapsulation for the forecast data :^)

---
+++ rainyshinycloudy/Forecast.swift
@@ -0,0 +1,47 @@
+//
+//  Forecast.swift
+//  rainyshinycloudy
+//
+//  Created by Fiona Carty on 1/3/17.
+//  Copyright © 2017 Fiona Carty. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class Forecast{
+    
+    var _date: String!
+    var _weatherType: String!
+    var _highTemp: String!
+    var _lowTemp: String!
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var highTemp: String {
+        if _highTemp == nil {
+            _highTemp = ""
+        }
+        return _highTemp
+    }
+    
+    var lowTemp: String {
+        if _lowTemp == nil {
+            _lowTemp = ""
+        }
+        return _lowTemp
+    }
+    
+}
