21578093698373dd379bb6d29151754b8347d021
fiona <fi.carty@gmail.com>
2017-01-08 17:25:56 -0800

    added comments to the Constants file

--- rainyshinycloudy/Constants.swift
+++ rainyshinycloudy/Constants.swift
@@ -8,8 +8,12 @@
 
 import Foundation
 
-typealias DownloadComplete = () -> () 
+typealias DownloadComplete = () -> ()
+
+// MARK: Current Weather URL
 
 let CURRENT_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&appid=2175963861a67d9dce42727bbc4cb849"
+
+// MARK: 10 day Forecast URL
 
 let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=10&mode=json&appid=2175963861a67d9dce42727bbc4cb849"
