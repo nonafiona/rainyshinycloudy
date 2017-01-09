56f79e846510ad2a95b9943689811adaf2cde356
fiona <fi.carty@gmail.com>
2017-01-08 17:23:17 -0800

    added comments to the Current Weather file.

--- rainyshinycloudy/CurrentWeather.swift
+++ rainyshinycloudy/CurrentWeather.swift
@@ -10,10 +10,15 @@ import UIKit
 import Alamofire
 
 class CurrentWeather {
+    
+    // MARK: Properties
+    
     var _cityName: String!
     var _date: String!
     var _weatherType: String!
     var _currentTemp: Double!
+    
+    // MARK: Data Encapsulation
     
     var cityName: String {
         if _cityName == nil {
@@ -50,32 +55,41 @@ class CurrentWeather {
         return _currentTemp
     }
     
+    // MARK: Downloading Current Weather 
+    
     func downloadWeatherDetails(completed: @escaping DownloadComplete) {
         //Alamofire download
         Alamofire.request(CURRENT_WEATHER_URL).responseJSON { response in
             
             let result = response.result
             
+            // MARK: Result Dictionary
+            
             if let dict = result.value as? Dictionary<String, AnyObject> {
                 
+                // MARK: City Name
                 if let name = dict["name"] as? String {
                     self._cityName = name.capitalized
-                    
                 }
+                
+                // MARK: Weather Dictionary
                 
                 if let weather = dict["weather"] as? [Dictionary<String,AnyObject>] {
                     
+                    // MARK: Main Weather Condition
                     if let main = weather[0]["main"] as? String {
-                        self._weatherType = main.capitalized 
-                        
+                        self._weatherType = main.capitalized
                     }
                 }
                 
+                // MARK: Main Dictionary
+                
                 if let main = dict["main"] as? Dictionary<String,AnyObject> {
                     
+                    // MARK: Current Temperature
                     if let currentTemperature = main["temp"] as? Double {
-                        let kelvinToFarenheitPreDivision = (currentTemperature * (9/5)-459.67)
                         
+                        let kelvinToFarenheitPreDivision = (currentTemperature * (9/5)-459.67)
                         let kelvinToFarenheit = Double(round(10 * kelvinToFarenheitPreDivision/10))
                         
                         self._currentTemp = kelvinToFarenheit
